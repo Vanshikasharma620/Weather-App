8fa71c00b90baf1a2fab93474b3e0b7774205ff7
Vanshikasharma620 <sharmavanshika620@gmail.com>
2021-09-18 17:21:48 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 async function dataa(event){
+    const showcity=document.getElementById("showcity");
     const weather=document.getElementById("weather");
     const temp=document.getElementById("temp");
     const minmax=document.getElementById("min");
@@ -10,11 +11,14 @@ async function dataa(event){
     console.log(city);
     const det= await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=5ea48567760b12859fe7fa46e3abb151");
     const respdata= await det.json();
-    weather.innerText=respdata["weather"][0]["description"];
-    temp.innerText="TEMP TODAY :: " + respdata["main"]["temp"];
-    maxmin.innerText="   MAX TEMP :: " + respdata["main"]["temp_max"];
-    minmax.innerText="MIN TEMP :: " + respdata["main"]["temp_min"] ;
-    wind.innerText="WIND TODAY :: " + respdata["wind"]["speed"];
-   
-    // console.log(respdata["main"]["temp"]);
+    showcity.innerText=respdata["name"];
+    weather.innerText= "Weather :: " +respdata["weather"][0]["description"];
+    temp.innerText="TEMP TODAY :: " + to_cel(respdata["main"]["temp"])+ " °C";
+    maxmin.innerText="   MAX TEMP :: " + to_cel(respdata["main"]["temp_max"])+ " °C";
+    minmax.innerText="MIN TEMP :: " + to_cel(respdata["main"]["temp_min"]) + " °C";
+    wind.innerText="WIND TODAY :: " + respdata["wind"]["speed"] + " m/s";
+}
+
+function to_cel(tempp){
+    return (tempp-273.15).toPrecision(3);
 }
